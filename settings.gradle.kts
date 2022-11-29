35373abe701c4f3de5f6c2fa05d094adc19c0e8c
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2022-11-29 13:41:53 +0300

    Enable the Gradle Enterprise plug-in for GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,22 @@
-
 rootProject.name = "test-kotlin-js-react"
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
